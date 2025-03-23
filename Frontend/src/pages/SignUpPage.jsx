b08419efa6c10c4a5712c7c62d59d1510d43ae74
Antarikshya Dash <dashantarikshya@gmail.com>
2025-03-23 16:43:27 +0530

    Commiting before experimenting

--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore.js';
-import { MessageSquare,User } from "lucide-react";
+import { Mail, MessageSquare, User } from "lucide-react";
 
-const SignUpPage = ()=> {
+const SignUpPage = () => {
 
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
@@ -11,9 +11,9 @@ const SignUpPage = ()=> {
     password: "",
   });
 
-  console.log("\n\n",useAuthStore(),"\n\n");
+  console.log("\n\n", useAuthStore(), "\n\n");
 
-  const {signUp, isSigningUp} = useAuthStore();
+  const { signUp, isSigningUp } = useAuthStore();
 
   const validateForm = () => { };
   const handleSubmit = (e) => {
@@ -52,12 +52,54 @@ const SignUpPage = ()=> {
                   <User className="size-5 text-base-content/40" />
                 </div>
 
+                <input
+                  type="text"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder='Joe Swanson'
+                  value={formData.fullName}
+                  onChange={(e) => { setFormData({ ...formData, fullName: e.target.value }) }}
+                />
+              </div>
+
+            </div>
+
+            <div className="form-control">
+              <label className='label'>
+                <span className='label-text font-medium'>Email</span>
+              </label>
+
+              <div className='relative'>
+                <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                  <User className="size-5 text-base-content/40" />
+                </div>
+
+                <input
+                  type="email"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder='you@example.com'
+                  value={formData.email}
+                  onChange={(e) => { setFormData({ ...formData, email: e.target.value }) }}
+                />
+              </div>
+
+            </div>
+
+            <div className="form-control">
+              <label className='label'>
+                <span className='label-text font-medium'>Password</span>
+              </label>
+
+              <div className='relative'>
+                <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                  <Mail className="size-5 text-base-content/40" />
+                </div>
+
                 <input 
                 type="text" 
                 className={`input input-bordered w-full pl-10`}
                 placeholder='Joe Swanson'
-                value={formData.fullName}
-                onChange={(e)=>{setFormData({...formData,fullName:e.target.value})}}
+                value={formData.password}
+                onChange={(e)=>{setFormData({...formData,password:e.target.value})}}
                  />
               </div>
 
